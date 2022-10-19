5bc2c8a711de1c519fc941694e2d9f4e65ed448b
Valentine2504 <uchechukwuvalentine47@gmail.com>
2022-10-20 00:08:13 +0100

    new file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include <main.h>
+
+/**
+ * a program that writes a function that prints
+ * 10 times the alphabet in lowercase, followed by 
+ * a new line
+ */
+void print_alphabet_x10(void);
+          char = N;
+	  for (N="a"; N<=10; N++);
+{
+	   _putchar(N="letter");
+	   
+}
+
+            _putchar("\n");
+
