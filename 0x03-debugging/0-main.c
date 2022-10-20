4ad39bab48300419fb2f6fe114ce53ee264b9d6f
Valentine2504 <uchechukwuvalentine47@gmail.com>
2022-10-21 00:40:22 +0100

    created 0-main.c and main.h file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * this is a file to test that the function positive_o
+ * r_negative gives the correct output when given a ca
+ * se of zero
+ */
+int main (void)
+{
+	int i;
+	i=50;
+	positive_or_negative(i);
+
+	return (0);
+}
+
+
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,4 @@
+/**
+*this is a readme file for 0x03-debugging repository
+*/
+
