eb6d8bac5775c647e8783cbc906c8e97dd08fcd2
Valentine2504 <uchechukwuvalentine47@gmail.com>
2022-10-26 02:19:58 +0100

    my first work that i did on my own

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code 
+ *
+ * Return: Always 0.
+ */
+void reset_to_98(int *n);
+{
+    int n;
+
+    n = 402;
+    _putchar("n=%d\n",&n, n);
+    int *p_n=98;
+    _putchar("n=%d\n",&n, n);
+    return (0);
+}
